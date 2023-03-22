f7df428b3b9acae655889c43be39307c0692169d
Laura McGowan <prime.lmcg@gmail.com>
2023-03-22 17:48:48 -0500

    file fixes

--- README.md
+++
@@ -1,2 +0,0 @@
-# seb-code-challenge
-An app the utilizes Github's API to return the top 5 starred repos for a user-supplied programming language.
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react';
+
+function App() {
+  // const [language, setLanguage] = useState("");
+  // const [repos, setRepos] = useState([]);
+
+
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default App;
