7ea0238bdfa38beabfdb00a537611cf2be111a68
Laura McGowan <prime.lmcg@gmail.com>
2023-03-22 19:09:33 -0500

    state is set, token brought in, end-point determined

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 
 function App() {
-  // const [language, setLanguage] = useState("");
-  // const [repos, setRepos] = useState([]);
+  const [language, setLanguage] = useState("");
+  const [repos, setRepos] = useState([]);
+
+
+  //Handle the language provided by the user
+  const handleLanguageInput = (event) => {
+    setLanguage(event.target.value);
+  }
+
+  //Hadle the submit button
+  const handleSubmitButton = (event) => {
+    event.preventDefault();
+  }
+
+  //API Url
+  const apiEndPoint = `https://api.github.com/search/repositories?q=language:{language}`;
+
+  //Request header with my token
+  const headers = {
+    Authorization: `token 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`
+  }
 
 
   return (
